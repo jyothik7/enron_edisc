4249a78864519e360fb68518d776e130036f0a60
arjun.dasgupta <arjun.dasgupta@opower.com>
2013-03-02 12:34:41 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-=== EDiscovery ===
+# Enron EDiscovery
+
+## Requirements
+Needs perl HTML::Entities lib
+```shell
+sudo perl -MCPAN -e 'shell'
+cpan[1]>  install HTML::Parser 
+```
--- lib/parseToTags.pl
+++ lib/parseToTags.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
+use HTML::Entities ();
 use strict;
-# use HTML::Entities ();
 
 # declare "only use once" flags
 my $has_date = 0;
@@ -15,23 +15,43 @@ my $has_x_zlid = 0;
 
 while (my $line=<>) {
     if( !$has_date && ($line =~ m/Date:(.*)\r/) ) {
-        print "<DATE>".$1."</DATE>"."\n";
+        print HTML::Entities::encode("<DATE>".$1."</DATE>"."\n");
         $has_date = 1;
     };
     
     if( !$has_from && ($line =~ m/From:(.*)\r/) ) {
-        print "<FROM>".$1."</FROM>"."\n";
+        print HTML::Entities::encode("<FROM>".$1."</FROM>"."\n");
         $has_from = 1;
     };
     
     if( !$has_to && ($line =~ m/To:(.*)\r/) ) {
-        print "<TO>".$1."</TO>"."\n";
+        print HTML::Entities::encode("<TO>".$1."</TO>"."\n");
         $has_to = 1;
     };
     
     if( !$has_cc && ($line =~ m/Cc:(.*)\r/) ) {
-        print "<CC>".$1."</CC>"."\n";
+        print HTML::Entities::encode("<CC>".$1."</CC>"."\n");
         $has_cc = 1;
+    };
+    
+    if( !$has_bcc && ($line =~ m/Bcc:(.*)\r/) ) {
+        print HTML::Entities::encode("<BCC>".$1."</BCC>"."\n");
+        $has_bcc = 1;
+    };
+    
+    if( !$has_subject && ($line =~ m/Subject:(.*)\r/) ) {
+        print HTML::Entities::encode("<SUBJECT>".$1."</SUBJECT>"."\n");
+        $has_subject = 1;
+    };
+    
+    if( !$has_x_sdoc && ($line =~ m/X-SDOC:(.*)\r/) ) {
+        print HTML::Entities::encode("<XSDOC>".$1."</XSDOC>"."\n");
+        $has_x_sdoc = 1;
+    };
+    
+    if( !$has_x_zlid && ($line =~ m/X-ZLID:(.*)\r/) ) {
+        print HTML::Entities::encode("<XZLID>".$1."</XZLID>"."\n");
+        $has_x_zlid = 1;
     };
     
     # print HTML::Entities::encode($line);
