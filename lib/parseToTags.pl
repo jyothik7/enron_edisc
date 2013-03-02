6cb856d40f5dc5b1f2c1242bcd6fd745cea6111c
arjun.dasgupta <arjun.dasgupta@opower.com>
2013-03-02 09:58:03 -0500

    first working verion of the perl script, little steps

--- lib/parseToTags.pl
+++ lib/parseToTags.pl
@@ -1,20 +1,11 @@
 #!/usr/bin/perl
 
-use 5.010;
-use strict;
-use warnings;
-
-  my $data = 'From: Xochitl-Alexis Velasco
-To: Mark Knippa, Mike D Smith, Gerald Nemec, Dave S Laipple, Bo Barnwell
-Cc: Melissa Jones, Iris Waser, Pat Radford, Bonnie Shumaker
-Subject: Finalize ECS/EES Master Agreement';
-
-  my @values = split(':', $data);
-  
-
-for my $elem (@values) {
-    state $index;
-    say "At index ", $index++, ", $elem";
+# use HTML::Entities ();
+while ($line=<>) {
+    if($line =~ /^Date:/) {
+        print $line;
+    };
+    
+    
+    # print HTML::Entities::encode($line);
 }
-
-exit 0;
